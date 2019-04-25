9c01809d8d39f3098affed26dba7926784b1d08a
root <root@Robinson.localdomain>
2019-04-25 02:41:40 -0500

    updated java files

---
+++ src/test/java/hello/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class SimpleUnitTest {
+    @Test
+    public void testOperation() {
+        assertTrue(true);
+        assertFalse(false);
+    }
+}
