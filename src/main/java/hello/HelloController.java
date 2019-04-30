6b64a617449ee8e605e2626859998d03da9a1838
Luis <luis.urrelo.1980@gmail.com>
2019-04-30 15:01:17 -0500

    updated java class

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring";
+        return "Greetings from Spring Boot! - Betoven";
     }
 }
