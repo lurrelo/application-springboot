1ce8e23edac1f46f72ccf0c62026c9b6afdc94e2
Luis <luis.urrelo.1980@gmail.com>
2019-04-29 23:25:43 -0500

    updated file

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,7 +2,7 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController {
